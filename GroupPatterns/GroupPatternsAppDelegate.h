625becb51ae3b9c0a9e681f0c64dafc0f59013b0
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2011-08-14 12:52:58 -0700

    Initial Commit

---
+++ GroupPatterns/GroupPatternsAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  GroupPatternsAppDelegate.h
+//  GroupPatterns
+//
+//  Created by Jeremy Lightsmith on 8/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GroupPatternsAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ GroupPatterns/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  GroupPatterns
+//
+//  Created by Jeremy Lightsmith on 8/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
